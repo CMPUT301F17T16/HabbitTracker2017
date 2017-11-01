1ed6b8adc12a0f1ad9f1ed2f5266485aab69cef2
jocowan <jcowan@ualberta.ca>
2017-11-01 17:23:35 -0600

    Readded main java file

---
+++ app/src/main/java/com/example/habittracker2017/HabitTracker2017Activity.java
@@ -0,0 +1,13 @@
+package com.example.habittracker2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HabitTracker2017Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_habit_tracker2017);
+    }
+}
