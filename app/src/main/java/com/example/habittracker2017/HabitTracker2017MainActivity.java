5231fda2bf08e82ab317eb7cb9c28b8684a3e02d
jocowan <jcowan@ualberta.ca>
2017-11-01 20:16:06 -0600

    changed main name

--- app/src/main/java/com/example/habittracker2017/HabitTracker2017MainActivity.java
+++ app/src/main/java/com/example/habittracker2017/HabitTracker2017MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.habittracker2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
-public class HabitTracker2017Activity extends AppCompatActivity {
-
+public class HabitTracker2017MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_tracker2017);
+
     }
 }
