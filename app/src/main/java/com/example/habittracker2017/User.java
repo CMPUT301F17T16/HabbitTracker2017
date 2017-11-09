43c768fd7798ae021ff7ce776a1be8c25e9d42da
almacken <almacken@ualberta.ca>
2017-11-08 17:36:01 -0700

    Added a few methods to User

--- app/src/main/java/com/example/habittracker2017/User.java
+++ app/src/main/java/com/example/habittracker2017/User.java
@@ -28,6 +28,46 @@ public class User {
         requests = new ArrayList<String>();
     }
 
+    /**
+     * Adds a new habit to this user.
+     * @param habit The habit to add.
+     */
+    public void addHabit(Habit habit){
+        habits.add(habit);
+    }
+
+    /**
+     * Deletes a habit.
+     * @param habit The habit to delete.
+     */
+    public void deleteHabit(Habit habit){
+        habits.remove(habit);
+    }
+
+    /**
+     * Adds a username to following.
+     * @param name The username to add.
+     */
+    public void addFollowing(String name){
+        following.add(name);
+    }
+
+    /**
+     * Adds a username to follower.
+     * @param name The username to add.
+     */
+    public void addFollower(String name){
+        followers.add(name);
+    }
+
+    /**
+     * Adds a username to request.
+     * @param name The username to add.
+     */
+    public void addRequest(String name){
+        requests.add(name);
+    }
+
     public String getName() {
         return name;
     }
