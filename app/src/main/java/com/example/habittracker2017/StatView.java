1c57435784828a10e40a94d7287ff1463e405371
Jacy Mark <jacy@dragonfruitcode.com>
2017-12-01 16:06:07 -0700

    updated colors and design for pie chart

--- app/src/main/java/com/example/habittracker2017/StatView.java
+++ app/src/main/java/com/example/habittracker2017/StatView.java
@@ -11,9 +11,25 @@ import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
+/**
+ * Created by jmark on 2017-12-01.
+ * Passed start date of habit and calculates all scheduled days in between start date and end date
+ * compares to habit events, if habit event exists for that date put it in completed array
+ * if no habit event found for it put it in missed
+ * pie chart vals are completed/(completed + missed) and missed/(completed+missed)
+ *
+ * todo: controller class
+ * todo: calendar view
+ * todo: changeable dates
+ */
+
 public class StatView extends AppCompatActivity {
+
+    Date currentDay = Calendar.getInstance().getTime();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +44,12 @@ public class StatView extends AppCompatActivity {
         entries.add(new PieEntry(50f, "Completed"));
         entries.add(new PieEntry(50f, "Missed"));
 
-        for (int c : ColorTemplate.VORDIPLOM_COLORS)
-            colors.add(c);
+        chart.setHoleRadius(0f);
+        chart.getDescription().setEnabled(false);
 
-        colors.add(ColorTemplate.getHoloBlue());
+        colors.add(Color.rgb(0,153,51));//green for complete
+        colors.add(Color.rgb(244,66,66));//red for missed
+
 
         PieDataSet set = new PieDataSet(entries,"Habits");
         set.setColors(colors);
